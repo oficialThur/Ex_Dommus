0172d4faa5ad695e13613559e71d8147b14442a7
oficialThur <oficialthurhenrique@gmail.com>
2025-12-16 14:44:19 -0300

    Arthur Henrique: passo1 postman

--- conflito-1.md
+++
@@ -1,3 +0,0 @@
-ewqeqeqewqwqeqwe
-swqaeqqeqw1
-o
--- projectDockerPhpNginxMySql/app/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '<h1>Ambiente Docker Compose (Nginx, PHP-FPM, MySQL) - Funcionando!</h1>';
-echo '<p>Versão do PHP: ' . phpversion() . '</p>';
